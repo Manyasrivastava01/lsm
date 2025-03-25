51d2d5e9660b2ee6c4ab3b5316186536b1a319fd
Keshav Dokania <keshavdokania2262@gmail.com>
2025-03-25 18:21:26 +0530

    img 9 10 11 12

--- src/components/reading/Books.jsx
+++ src/components/reading/Books.jsx
@@ -8,10 +8,10 @@ import Book5Image from "../../assets/book5.jpeg";
 import Book6Image from "../../assets/book6.jpeg";
 import Book7Image from "../../assets/book7.jpeg";
 import Book8Image from "../../assets/book8.jpeg";
-import Book9Image from "../../assets/book9.jpeg";
-import Book10Image from "../../assets/book10.jpeg";
-import Book11Image from "../../assets/book11.jpeg";
-import Book12Image from "../../assets/book12.jpeg";
+import Book9Image from "../../assets/book9.jpg";
+import Book10Image from "../../assets/book10.jpg";
+import Book11Image from "../../assets/book11.jpg";
+import Book12Image from "../../assets/book12.jpg";
 import samplePDF1 from "../../assets/pdf1.pdf"; // Import the PDF file
 
 function BooksSection() {
@@ -30,9 +30,21 @@ function BooksSection() {
     { image: Book7Image, title: "Pregnancy Day by Day", pdf: samplePDF1 },
     { image: Book8Image, title: "The Birth Partner", pdf: samplePDF1 },
     { image: Book9Image, title: "Natural Childbirth", pdf: samplePDF1 },
-    { image: Book10Image, title: "Happiest Baby on the Block", pdf: samplePDF1 },
-    { image: Book11Image, title: "The Ima's Guide to Pregnancy", pdf: samplePDF1 },
-    { image: Book12Image, title: "Childbirth and the Newborn", pdf: samplePDF1 },
+    {
+      image: Book10Image,
+      title: "Happiest Baby on the Block",
+      pdf: samplePDF1,
+    },
+    {
+      image: Book11Image,
+      title: "The Ima's Guide to Pregnancy",
+      pdf: samplePDF1,
+    },
+    {
+      image: Book12Image,
+      title: "Childbirth and the Newborn",
+      pdf: samplePDF1,
+    },
   ];
 
   const booksPerPage = 3;
@@ -96,58 +108,60 @@ function BooksSection() {
         </button>
         <div className="flex overflow-hidden">
           <AnimatePresence>
-            {books.slice(currentIndex, currentIndex + booksPerPage).map((book, index) => {
-              let scale, y, x;
+            {books
+              .slice(currentIndex, currentIndex + booksPerPage)
+              .map((book, index) => {
+                let scale, y, x;
 
-              if (!swiped) {
-                scale = 1;
-                y = 0;
-                x = 0;
-              } else {
-                if (index === 1) {
-                  scale = 1.1;
-                  y = -10;
+                if (!swiped) {
+                  scale = 1;
+                  y = 0;
                   x = 0;
                 } else {
-                  scale = 0.9;
-                  y = 0;
-                  x = index === 0 ? -100 : 100;
+                  if (index === 1) {
+                    scale = 1.1;
+                    y = -10;
+                    x = 0;
+                  } else {
+                    scale = 0.9;
+                    y = 0;
+                    x = index === 0 ? -100 : 100;
+                  }
                 }
-              }
 
-              return (
-                <motion.div
-                  key={index}
-                  ref={bookRefs.current[currentIndex + index]}
-                  className="flex flex-col items-center border p-4 rounded-lg shadow-md m-2 h-72 w-72 transition-opacity duration-500"
-                  initial={{ opacity: 0, x: 100, y: 20 }}
-                  animate={{
-                    opacity: 1,
-                    x: 0,
-                    y: -10,
-                    scale,
-                  }}
-                  exit={{ opacity: 0, x, scale: 0.8 }}
-                  transition={{ duration: 0.5 }}
-                  whileHover={{ scale: 1.05, y: -5 }}
-                >
-                  <img
-                    className="h-40 w-40 object-cover rounded-md mb-3"
-                    src={book.image}
-                    alt={`Book ${index + 1}`}
-                  />
-                  <h4 className="text-lg font-semibold mb-4 text-center h-16">
-                    {book.title}
-                  </h4>
-                  <button
-                    className="bg-[#F38FB1] py-2 rounded-full font-bold text-white w-full"
-                    onClick={() => openModal(book.pdf)} // Open the modal with the corresponding PDF
+                return (
+                  <motion.div
+                    key={index}
+                    ref={bookRefs.current[currentIndex + index]}
+                    className="flex flex-col items-center border p-4 rounded-lg shadow-md m-2 h-72 w-72 transition-opacity duration-500"
+                    initial={{ opacity: 0, x: 100, y: 20 }}
+                    animate={{
+                      opacity: 1,
+                      x: 0,
+                      y: -10,
+                      scale,
+                    }}
+                    exit={{ opacity: 0, x, scale: 0.8 }}
+                    transition={{ duration: 0.5 }}
+                    whileHover={{ scale: 1.05, y: -5 }}
                   >
-                    Read More
-                  </button>
-                </motion.div>
-              );
-            })}
+                    <img
+                      className="h-40 w-40 object-cover rounded-md mb-3"
+                      src={book.image}
+                      alt={`Book ${index + 1}`}
+                    />
+                    <h4 className="text-lg font-semibold mb-4 text-center h-16">
+                      {book.title}
+                    </h4>
+                    <button
+                      className="bg-[#F38FB1] py-2 rounded-full font-bold text-white w-full"
+                      onClick={() => openModal(book.pdf)} // Open the modal with the corresponding PDF
+                    >
+                      Read More
+                    </button>
+                  </motion.div>
+                );
+              })}
           </AnimatePresence>
         </div>
         <button
@@ -163,10 +177,13 @@ function BooksSection() {
       {isModalOpen && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
           <div className="bg-white p-4 rounded-lg">
-            <button className="absolute top-2 right-2 text-black" onClick={closeModal}>
+            <button
+              className="absolute top-2 right-2 text-black"
+              onClick={closeModal}
+            >
               X
             </button>
-            <iframe 
+            <iframe
               src={isModalOpen} // Display the PDF inside an iframe
               width="600"
               height="400"
